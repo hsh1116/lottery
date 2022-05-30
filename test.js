7eecaf7056511c0db12c970571a1b12c643f4522
hsh1116 <83573703+hsh1116@users.noreply.github.com>
2022-05-30 11:39:28 +0900

    Add files via upload

--- test.js
+++ test.js
@@ -1,10 +1,14 @@
 console.log('Hello Codeit!')
 
-print('Hello World!!!')
+//print()
 
 //
-var input = prompt('입력받기', '메시지를 입력해주세요');
-//alert(input);
-document.write(input);
-//-`document.forms[0].reset();
-document.reset();
+// var input = prompt('입력받기', '메시지를 입력해주세요');
+// //alert(input);
+// document.write(input);
+// //-`document.forms[0].reset();
+// document.reset();
+
+
+
+document.write('Hello World!!!');
